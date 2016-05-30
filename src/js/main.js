f17ac485fe583fcd60389b6cbe135e772700f0dc
Asher Alley <asher.asheralley@gmail.com>
2016-05-31 10:06:14 +1200

    added all files

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+var responsiveNav = require('responsive-nav');
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	var nav = responsiveNav('.nav-collapse', {
+		transition: 400,
+		insert: 'after'
+	});
+});
