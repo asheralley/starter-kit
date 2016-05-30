8c0750e40e729f6f5824fa32417f2f65ab6e8ea3
Asher Alley <asher.asheralley@gmail.com>
2016-05-31 10:32:41 +1200

    added all files

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,16 @@
+// Require modules
+
 var responsiveNav = require('responsive-nav');
+var Flickity = require('flickity');
 
-document.addEventListener('DOMContentLoaded', function() {
+// Scripts
 
-	var nav = responsiveNav('.nav-collapse', {
-		transition: 400,
-		insert: 'after'
-	});
+var nav = responsiveNav('.nav-collapse', {
+	transition: 400,
+	insert: 'after'
+});
+
+var flickity = new Flickity('.flickity', {
+	contain: true,
+	wrapAround: true
 });
